fcbd3685cba0302cde2dc7ad728d9e1b75dda3ee
Marcel Miranda <m@reaktivo.com>
2018-07-04 17:22:53 +0200

    Second try

---
+++ src/index.test.ts
@@ -0,0 +1,22 @@
+/// <reference types="jest" />
+import asyncFlow from ".";
+
+describe("async-flow", () => {
+  it("should return a function", () => {
+    expect(asyncFlow(() => {})).toBeInstanceOf(Function);
+  });
+
+  it("should flow", async () => {
+    const a = jest.fn(a => a + a);
+    const b = jest.fn(b => b * b);
+    const result = await asyncFlow(a, b)(2);
+    expect(result).toBe(16);
+  });
+
+  it("should support async flows", async () => {
+    const a = jest.fn(a => a + a);
+    const b = jest.fn(b => Promise.resolve(b * b * b));
+    const result = await asyncFlow(a, b)(2);
+    expect(result).toBe(64);
+  });
+});
